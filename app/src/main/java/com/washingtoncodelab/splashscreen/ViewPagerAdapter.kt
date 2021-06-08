676bc34c311d5c1c49387c626a4ce4e832e3c5e3
peter ngotho <sammykamau772@mail.com>
2021-06-08 14:16:38 -0400

    initial commit

---
+++ app/src/main/java/com/washingtoncodelab/splashscreen/ViewPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.washingtoncodelab.splashscreen
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import kotlin.contracts.Returns
+
+class ViewPagerAdapter(list: ArrayList<Fragment>, fragmentManager: FragmentManager,lifecycle: Lifecycle):FragmentStateAdapter(fragmentManager, lifecycle) {
+    val fragmentList :ArrayList<Fragment> = list
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
